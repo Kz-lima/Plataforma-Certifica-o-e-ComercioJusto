ddd94cdbebe07bb78a201e4806e3020a0e60a103
daironromano <daironromano@gmail.com>
2026-01-15 17:58:42 -0300

    feat: css do cabeçalho adicionado

--- amazonia_marketing/plataforma_certificacao/models.py
+++ amazonia_marketing/plataforma_certificacao/models.py
@@ -7,7 +7,6 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-
 class Certificacoes(models.Model):
     id_certificacao = models.AutoField(primary_key=True)
     texto_autodeclaracao = models.TextField(blank=True, null=True)
